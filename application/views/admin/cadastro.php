f8f1bea1f5584fae33cbdf0e275aea8b32c062bc
IgorCS <igorweb08@gmail.com>
2019-02-19 11:52:08 -0300

    crud-Vue_cadastro

--- application/views/admin/cadastro.php
+++ application/views/admin/cadastro.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Mini-Crud com Bootstrap e CodeIgniter 3.0</title>   
+	<title>Mini-Crud com Bootstrap e CodeIgniter 3.0-Vue</title>   
      <link href="<?php echo base_url(); ?>includes/bootstrap/css/bootstrap/bootstrap.min.css" rel="stylesheet">
      <link href="<?php echo base_url(); ?>includes/bootstrap/css/bootstrap/bootstrap-theme.min.css" rel="stylesheet">	
 
